36d866b93bd6adcae5255c0cbf95120a5537b74d
MClaraiconic <crisscamargo419@gmail.com>
2026-05-31 18:46:14 -0300

    Delete Projeto-Integrador-Banco-de-dados-main directory

--- Projeto-Integrador-Banco-de-dados-main/menu.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="admin.php">Produtos</a></li>
-    <li><a href="cliente.php">Clientes</a></li>
-    <li><a href="pedido.php">Pedidos</a></li>
-    <li><a href="logout.php">Sair</a></li>
-</ul>
